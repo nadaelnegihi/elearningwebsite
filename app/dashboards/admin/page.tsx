b692d21f4f0262c25c5e880eb56df20b693beb9f
mostafaelsayed3 <168917121+mostafaelsayed3@users.noreply.github.com>
2024-12-22 21:26:55 +0200

    Delete app directory

--- app/dashboards/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AdminDashboard() {
-    return (
-      <div className="p-8">
-        <h1 className="text-2xl font-bold mb-4">Welcome, Admin</h1>
-        <p>Manage users, courses, and perform administrative tasks.</p>
-      </div>
-    );
-  }
-  
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import RegisterPage from "./auth/register/page";
-
-export default function HomePage() {
-  return <RegisterPage />;
-}
